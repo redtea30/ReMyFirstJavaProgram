5f7318a9000ec2841f2c0e02f02423b4998578d8
Neko <jkspstyu@outlook.com>
2023-08-01 10:42:19 +0800

    continue practice

--- Day01/src/DataConversion.java
+++ Day01/src/DataConversion.java
@@ -44,9 +44,36 @@ public class DataConversion {
 
     @Test
     public void test3() {
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = ++num1;
+        System.out.println(num1 + num2 + num3);
+        System.out.println(--num1 + ++num2 + ++num3);
+        System.out.println(num1 + num2 + num3);
 
 
     }
 
+    @Test
+    public void test4() {
+        //练习
+//        9. 判断一个数是否是偶数
+        int num1 = (int) (Math.random() * 100);
+        // math.random()生成double类型的数据，必须强转
+        if (num1 % 2 == 0) {
+            System.out.println("是偶数");
+        } else {
+            System.out.println("不是��数");
+        }
+
+
+//        10. 使用三元运算符计算两个整数中的较大值
+
+        int num2 = 10;
+        int num3 = 20;
+        System.out.println(num2 > num3 ? num2 : num3);
+
+
+    }
 
 }
---
+++ Day01/src/PrintPersonInfo.java
@@ -0,0 +1,17 @@
+/**
+ * ClassName:PrintPersonInfo
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Neko
+ * @Create 1/8/20239:07 am
+ * @Version 1.0
+ */
+public class PrintPersonInfo {
+    public void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
