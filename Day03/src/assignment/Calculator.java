f6817a27980501c2bc97239f310f9d93dceaac7d
Neko <jkspstyu@outlook.com>
2023-08-02 19:51:25 +0800

    continue practice

---
+++ Day03/src/assignment/Calculator.java
@@ -0,0 +1,57 @@
+package assignment;
+
+import java.util.Scanner;
+
+/**
+ * ClassName:Calculator
+ * Package: assignment
+ * Description:
+ *
+ * @Author Neko
+ * @Create 2/8/20235:13 pm
+ * @Version 1.0
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        //编写一个简单的计算器程序，要求用户输入两个数和操作符（+、-、*、/），然后根据操作符进行相应的运算，并输出结果。如果输入的运算符不是有效的运算符，则输出"无效的运算符"。
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入第一个数字");
+        int num1 = scanner.nextInt();
+        System.out.println("请输入第二个数字");
+        int num2 = scanner.nextInt();
+        System.out.println("请输入一个运算符（+-*/）");
+        String operator = scanner.next();
+
+        double result = 0;
+        boolean flag = true;
+         switch (operator) {
+            case "+": {
+                result = num1 + num2;
+                break;
+            }
+            case "-": {
+                result = num1 - num2;
+                break;
+            }
+            case "*": {
+                result = num1 * num2;
+                break;
+            }
+            case "/": {
+                result = num1 / num2;
+                break;
+            }
+            default:
+                flag = false;
+        }
+
+
+        if (flag==false){
+            System.out.println("你输入的有误");
+        }else{
+            System.out.println(result);
+        }
+
+    }
+}
---
+++ Day03/src/assignment/Season.java
@@ -0,0 +1,33 @@
+package assignment;
+
+import java.util.Scanner;
+
+/**
+ * ClassName:Season
+ * Package: assignment
+ * Description:
+ *
+ * @Author Neko
+ * @Create 2/8/20236:40 pm
+ * @Version 1.0
+ */
+public class Season {
+    public static void main(String[] args) {
+        //编写一个程序，根据用户输入的月份（1 到 12），输出该月份所属的季节。假设春季 是 3 到 5 月，夏季是 6 到 8 月，秋季是 9 到 11 月，冬季是 12、1 和 2月。如果输入 的月份超出了范围，输出"输入错误"。
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("请输入月份");
+        int month = scanner.nextInt();
+        switch (month) {
+            case 12, 1, 2 -> System.out.println("是冬季");
+            case 3, 4, 5 -> System.out.println("是春季");
+            case 6, 7, 8 -> System.out.println("是夏季");
+            case 9, 10, 11 -> System.out.println("是秋季");
+            default -> System.out.println("你输入的有误");
+
+        }
+
+
+    }
+}
