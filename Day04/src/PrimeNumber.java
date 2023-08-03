6b1363c653ab0e28f011c02fa7fe6b97fe79e741
Neko <jkspstyu@outlook.com>
2023-08-03 19:52:09 +0800

    continue practice

--- Day04/src/PrimeNumber.java
+++ Day04/src/PrimeNumber.java
@@ -9,9 +9,21 @@
  */
 public class PrimeNumber {
     public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {//初始值不可以设为0
+                if (i % j == 0) {
+                    count++;
+                    if (count > 2) {
+                        break;
+                    }
+                }
+            }
+            if (count == 2) {
+                System.out.println(i);
+            }
 
-
-
+        }
 
 
     }
