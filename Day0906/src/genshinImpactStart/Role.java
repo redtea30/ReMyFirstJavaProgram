71894203146064eceb56d05c0c0633b1914ebc2d
Neko <jkspstyu@outlook.com>
2023-09-06 15:13:39 +0800

    原神，启动！

---
+++ Day0906/src/genshinImpactStart/Role.java
@@ -0,0 +1,13 @@
+package genshinImpactStart;
+
+/**
+ * ClassName:Role
+ * Package: genshinImpactStart
+ * Description:
+ *
+ * @Author Neko
+ * @Create 6/9/20233:13 pm
+ * @Version 1.0
+ */
+public class Role {
+}
---
+++ Day0906/src/genshinImpactStart/RoleManage.java
@@ -0,0 +1,13 @@
+package genshinImpactStart;
+
+/**
+ * ClassName:RoleManage
+ * Package: genshinImpactStart
+ * Description:
+ *
+ * @Author Neko
+ * @Create 6/9/20233:13 pm
+ * @Version 1.0
+ */
+public class RoleManage {
+}
