ef1d633fc9d60633176c0c6f1ee1716442037725
Neko <jkspstyu@outlook.com>
2023-11-28 00:24:16 +0800

    尝试修复项目问题

---
+++ web/day1116/src/main/java/MainTest.java
@@ -0,0 +1,13 @@
+/**
+ * Package-name: PACKAGE_NAME
+ * Project-name:ReMyFirstJavaProgram
+ * Author: Neko
+ * 16-11-2023  14:00
+ * Describe：
+ */
+
+public class MainTest {
+    public static void main(String[] args) {
+
+    }
+}
