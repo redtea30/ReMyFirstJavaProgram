3e389a90eca5a502de5abb20c2fadc106520ea73
Neko <jkspstyu@outlook.com>
2024-01-02 19:22:23 +0800

    处理表单序号

--- web/day1226login/src/components/MyList.vue
+++ web/day1226login/src/components/MyList.vue
@@ -4,7 +4,7 @@
              :columns="columnsFromJson"
              :data-source="dataSource"
              bordered
-             :loading="loading"
+             size="small"
              :pagination="pagination"
     >
 
@@ -18,7 +18,7 @@
   </div>
   <div>
     <!--    获取列表的列数-->
-    <span>默认展示数据：总数{{ contentNumbers }}</span>
+    <span>默认展示数据：总数{{ contentNumbers }} ，当前页{{ currentPage }}，每页显示{{ pageSize }}</span>
   </div>
 </template>
 
@@ -30,58 +30,45 @@ import {ref, computed} from 'vue';
 // 将导入的数据和列定义转换为响应式对象
 const dataSource = ref(Content);
 const columnsFromJson = ref(Title);
-// 数据列数
+// 获取数据表列总数
 const contentNumbers = computed(() => dataSource.value.length);
+
+// 当前页和每页显示的数量
+const currentPage = ref(1);
+const pageSize = ref(10);  // 或者使用 pagination.defaultPageSize
+
+// 计算行数的方法
+const calculateRowNumber = (index) => {
+  return (currentPage.value - 1) * pageSize.value + index + 1;
+};
+
 // 添加序号列
 const indexColumn = {
   title: '序号',
-  dataIndex: 'index',
-  key: 'index',
-  // 使用 scopedSlots 自定义渲染内容（如果需要）
-  scopedSlots: {customRender: 'serial'},
+  dataIndex: 'serial',
+  key: 'serial',
+  customRender: ({index}) => calculateRowNumber(index),
 };
-
 // 将序号列添加到列的开始位置
 columnsFromJson.value.unshift(indexColumn);
-
-const columns = computed(() => {
-  return columnsFromJson.value.map((col, index) => {
-    // 如果是序号列，添加序号逻辑
-    if (col.dataIndex === 'index') {
-      return {
-        ...col,
-        customRender: (_, record, rowIndex) => `${rowIndex + 1}`, // 这里 rowIndex + 1 就是序号
-      };
-    }
-    return col;
-  });
-});
-const deleteRow = (index) => {
-  dataSource.value.splice(index, 1);
-}
-// 添加操作列
-const operationColumn = {
-  title: '操作',
-  dataIndex: 'index',
-  key: 'index',
-  // 使用 scopedSlots 自定义渲染内容（如果需要）
-  scopedSlots: {customRender: 'serial'},
-};
-
-columnsFromJson.value.push(operationColumn);
-
-const loading = ref(false);
 
 const pagination = ref({
   position: ["bottomLeft"],
   showSizeChanger: true,
-  pageSizeOptions: [1, 2, 5, 10],
-  defaultPageSize: 2,
-  // 底下的直接写死了，不能用
-  // pageSize: 2,
-  // current: 1,
+  pageSizeOptions: [10, 20, 50],
+  defaultPageSize: 10,
   showQuickJumper: true,
+  currentPage,
+  pageSize,
+  onChange: handleTableChange,
+  onShowSizeChange: handleTableChange,
 });
+
+// 处理表格变化事件
+function handleTableChange(newPage, newPageSize) {
+  currentPage.value = newPage;
+  pageSize.value = newPageSize;
+}
 
 // todo 多选，不会写，有问题
 </script>
